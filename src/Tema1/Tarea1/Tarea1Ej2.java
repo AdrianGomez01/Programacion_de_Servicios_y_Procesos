5b92de198828e7db00c3a43237c6ce6b48e80369
Adrian Gomez <adrian.gomez.garlito@gmail.com>
2023-10-04 18:57:41 +0200

    tarea1

--- src/Tema1/Tarea1/Tarea1Ej2.java
+++ src/Tema1/Tarea1/Tarea1Ej2.java
@@ -1,4 +1,4 @@
-package Tarea1;
+package Tema1.Tarea1;
 
 import java.io.IOException;
 
@@ -6,14 +6,7 @@ public class Tarea1Ej2 {
 
     public static void main(String[] args) {
 
-        if (args.length <= 0) {
-            System.out.println("Debe indicarse comando a ejecutar.");
-            System.exit(1);
-        }
-
-        ProcessBuilder pb = new ProcessBuilder(args);
-        pb.inheritIO();
-
+        ProcessBuilder pb = new ProcessBuilder("ls");
         /**
          * Dentro de este bloque Try lanzamos el proceso y comprobamos si sigue en
          * ejecución, si es así esperaremos 3 segundos hasta volver a comprobarlo,
@@ -21,6 +14,7 @@ public class Tarea1Ej2 {
          */
         try {
             Process p = pb.start();
+
             while (true) {
                 if (!p.isAlive()) {
                     System.out.println("El proceso ha finalizado");
